040e5db5735bc662f0e9af180bf0f6354cdbc812
Sundar <91257478+Sundarleo11@users.noreply.github.com>
2022-05-26 08:59:23 +0530

    health check updated

--- src/main/java/com/authorization/config/WebSecurityConfig.java
+++ src/main/java/com/authorization/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    
 		httpSecurity.csrf().disable().cors().disable().authorizeRequests()
 		
-		.antMatchers("/authenticate", "/authorize","/actuator/health","/console/**", "/**")
+		.antMatchers("/authenticate", "/authorize","/health-check"/actuator/health","/console/**", "/**")
 				.permitAll().anyRequest().authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
